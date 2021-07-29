f9ff676dbfa014264867e3c5fc2e8deb4fde5f8f
smoha020 <smoha020@ucr.edu>
2021-07-28 18:01:01 -0700

    created and added build folder

---
+++ client/build/precache-manifest.86425e9c18caaac58e35808d6ca5891e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a69652ed70e28549ed576fd9869deb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b128729947a61eb9b5",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3acfc1a2fb15869236a4",
+    "url": "/static/css/main.9977767a.chunk.css"
+  },
+  {
+    "revision": "37b128729947a61eb9b5",
+    "url": "/static/js/2.a7eef8b9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.a7eef8b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3acfc1a2fb15869236a4",
+    "url": "/static/js/main.143e3aea.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
